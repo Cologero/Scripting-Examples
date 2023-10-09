aa83431a5bc20689cc77804917b9aafbb44a4032
Cologero <lennonalvarez530@gmail.com>
2023-10-09 11:57:58 -0700

    save files

---
+++ Scripting Fundamentals/Assets/Scripts/Variables1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables1 : MonoBehaviour
+{
+    // Declaring varibles
+    public string PlayerName = "Lennon";
+    public int Gradelevel = 12;
+    public float GPA = 2.5f;
+
+    // Private Member/Global Varibles
+    private int favroritenumber = 1;
+    private string favortiemovie = "Inglouriousbasterds";
+    private int favoritenumber = 30;
+    private float Gasprice = 5.87f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
