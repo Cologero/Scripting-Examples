2647da63b19bc7d58968a65e472d74f91e4337a9
Cologero <lennonalvarez530@gmail.com>
2023-10-10 12:11:21 -0700

    Update Variables2.cs

--- Scripting Fundamentals/Assets/Scripts/Variables2.cs
+++ Scripting Fundamentals/Assets/Scripts/Variables2.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
-public class Variables2 : MonoBehaviour
+public class Quiz1codingchallenge : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        IncreseScore();
+        PrintPlayerName();
+        DecreaseHealth(20);
     }
+
+    // Declaring Variables
+    public int score = 0;
+    public string PlayerName = "Lennon";
+    public int Health = 100;
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void IncreseScore()
+    {
+        score = score + 10;
+        score += 10;
+    }
+
+    private void PrintPlayerName()
+    {
+        Debug.Log(PlayerName);
+    }
+
+    private void DecreaseHealth(int amount)
+    {
+        Health -= amount;
+    }
+    
 }
